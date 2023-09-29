12e7a4d029bb7fce207cf06def52cb08936a6caf
ivermektin <143585246+ivermektin@users.noreply.github.com>
2023-09-29 08:55:29 -0600

    Refactor URL

--- src/main/java/org/ivermektin/caledonia/interfaces/webInterfaces/BlurbManager.java
+++ src/main/java/org/ivermektin/caledonia/interfaces/webInterfaces/BlurbManager.java
@@ -3,7 +3,8 @@ package org.ivermektin.caledonia.interfaces.webInterfaces;
 import org.ivermektin.caledonia.interfaces.webInterfaces.lowLevel.RawWebManager;
 
 public class BlurbManager extends RawWebManager{
+    private static String BLURB_SITE = "https://raw.githubusercontent.com/ivermektin/caledonia/main/launcher_webinfo/blurb.txt";
     public static String getBlurb(){
-        return pullFromSite("https://raw.githubusercontent.com/ivermektin/caledonia/main/launcher_webinfo/blurb.txt").get(0);
+        return pullFromSite(BLURB_SITE).get(0);
     }
 }
