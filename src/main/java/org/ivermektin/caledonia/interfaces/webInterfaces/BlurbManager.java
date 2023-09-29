0ecd7eb5b18cd299731f9f98525e490abb013128
ivermektin <143585246+ivermektin@users.noreply.github.com>
2023-09-29 08:55:59 -0600

    Change Variable Name

--- src/main/java/org/ivermektin/caledonia/interfaces/webInterfaces/BlurbManager.java
+++ src/main/java/org/ivermektin/caledonia/interfaces/webInterfaces/BlurbManager.java
@@ -3,8 +3,8 @@ package org.ivermektin.caledonia.interfaces.webInterfaces;
 import org.ivermektin.caledonia.interfaces.webInterfaces.lowLevel.RawWebManager;
 
 public class BlurbManager extends RawWebManager{
-    private static String BLURB_SITE = "https://raw.githubusercontent.com/ivermektin/caledonia/main/launcher_webinfo/blurb.txt";
+    private static String BLURB_URL = "https://raw.githubusercontent.com/ivermektin/caledonia/main/launcher_webinfo/blurb.txt";
     public static String getBlurb(){
-        return pullFromSite(BLURB_SITE).get(0);
+        return pullFromSite(BLURB_URL).get(0);
     }
 }
