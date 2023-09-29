dfb1a84a90c9160d3d11bbd2a8c7b28c41a09633
ivermektin <143585246+ivermektin@users.noreply.github.com>
2023-09-29 10:35:54 -0600

    Update dependency.

--- src/main/java/org/ivermektin/caledonia/interfaces/webInterfaces/BlurbManager.java
+++ src/main/java/org/ivermektin/caledonia/interfaces/webInterfaces/BlurbManager.java
@@ -1,8 +1,8 @@
 package org.ivermektin.caledonia.interfaces.webInterfaces;
 
-import org.ivermektin.caledonia.interfaces.webInterfaces.lowLevel.RawWebManager;
+import org.ivermektin.caledonia.interfaces.webInterfaces.lowLevel.RawWebService;
 
-public class BlurbManager extends RawWebManager{
+public class BlurbManager extends RawWebService{
     private static String BLURB_URL = "https://raw.githubusercontent.com/ivermektin/caledonia/main/launcher_webinfo/blurb.txt";
     public static String getBlurb(){
         return pullFromSite(BLURB_URL).get(0);
